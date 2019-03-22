bd85fd49b3cbdfd9a205f7f74003c361c54e2b2f
adam <adam@192.168.2.58>
2019-03-22 21:47:22 +0100

    cucumber part 2

--- src/test/java/pl/lait/Steps/LoginPageStepDefs.java
+++ src/test/java/pl/lait/Steps/LoginPageStepDefs.java
@@ -1,25 +1,81 @@
 package pl.lait.Steps;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.*;
+import cucumber.api.java.en.*;
+import pl.lait.pageObjects.LoginPage;
+import pl.lait.pageObjects.ReservationPage;
+import pl.lait.selenium.Init;
+
 public class LoginPageStepDefs {
-	
-	
+
+	WebDriver driver;
+	LoginPage lp;
+	ReservationPage rp;
+
+	@Before
+	public void bifor() {
+		driver = Init.getDriver();
+		lp = new LoginPage(driver);
+		rp = new ReservationPage();
+	}
+
 	@Given("^I open main page$")
 	public void i_open_main_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		// upewniam się, że jestem na właściwej stronie
+		String pageTitle = driver.getTitle();
+		Init.log("Current page title: " + pageTitle);
 	}
 
 	@When("^I fill the login form with login \"([^\"]*)\" and pass \"([^\"]*)\"$")
 	public void i_fill_the_login_form_with_login_and_pass(String arg1, String arg2) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		lp.goToLoginPage();
+		lp.loginAs(arg1, arg2);
 	}
 
 	@Then("^I should see the flight reservation form$")
 	public void i_should_see_the_flight_reservation_form() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+
 	}
 
+	@When("^I choose \"([^\"]*)\" passengers$")
+	public void i_choose_passengers(String arg1) throws Throwable {
+		rp.passCount(arg1);
+	}
+
+	@When("^I choose one way trip type$")
+	public void i_choose_one_way_trip_type() throws Throwable {
+		rp.oneWay();
+		driver.findElement(By.xpath("adfasdfadsfdfdfdfdsf")).click();
+	}
+
+	@When("^I start from \"([^\"]*)\" at month: \"([^\"]*)\" day \"([^\"]*)\"$")
+	public void i_start_from_at_month_day(String arg1, String arg2, String arg3) throws Throwable {
+		rp.from(arg1, arg2, arg3);
+	}
+
+	@When("^I end at \"([^\"]*)\" at month: \"([^\"]*)\" day \"([^\"]*)\"$")
+	public void i_end_at_at_month_day(String arg1, String arg2, String arg3) throws Throwable {
+		rp.to(arg1, arg2, arg3);
+	}
+
+	@When("^I i want to fly first class using \"([^\"]*)\"$")
+	public void i_i_want_to_fly_first_class_using(String arg1) throws Throwable {
+		rp.firstClass();
+		//TODO brak wyboru linii
+	}
+
+	@Then("^I submit first level of my flight resevation$")
+	public void i_submit_first_level_of_my_flight_resevation() throws Throwable {
+		rp.submit();
+	}
+
+	@After
+	public void endTest() {
+		Init.close();
+	}
 
 }
--- src/test/resources/pl/lait/Steps/loginPage.feature
+++ src/test/resources/pl/lait/Steps/loginPage.feature
@@ -1,9 +1,20 @@
-@tag
 Feature: Flight reservation
   I want to login and reserve some flight
 
-  @tag1
   Scenario: login to the app
     Given I open main page
     When I fill the login form with login "adamdz" and pass "qwe123"
     Then I should see the flight reservation form
+    
+    
+    
+  Scenario: reserve flight
+  	Given I open main page
+  	When I fill the login form with login "adamdz" and pass "qwe123"
+  	And I choose "2" passengers
+  	And I choose one way trip type
+  	And I start from "London" at month: "3" day "22"
+  	And I end at "Paris" at month: "3" day "25"
+  	And I i want to fly first class using "Pangea AirLines"
+  	Then I submit first level of my flight resevation
+  	
